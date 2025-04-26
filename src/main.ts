e3169419c1f26d6d5726a19036e0b804c2f5635d
Romel Rey Silva <sromelrey@gmail.com>
2025-04-26 20:01:31 +0800

    chore(swagger and validator): swagger and validator basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Nest JS Intro')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense('MIT License', '')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+  // Instanstiate  Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
